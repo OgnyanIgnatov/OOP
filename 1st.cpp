eac8f4e54c228ea9250bf97be48296e59b345c03
OgnyanIgnatov <125972711+OgnyanIgnatov@users.noreply.github.com>
2023-02-21 16:53:12 +0200

    Update 1st.cpp

--- 1st.cpp
+++ 1st.cpp
@@ -9,9 +9,11 @@ struct Time
     int second;
     Time get_diff(Time end_time)
     {
-        end_time.hour - hour;
-        end_time.minute - minute;
-        end_time.second - second;
+        Time diff;
+        diff.hour = end_time.hour - hour;
+        diff.minute = end_time.minute - minute;
+        diff.second = end_time.second - second;
+        return diff;
     }
 };
 
@@ -34,14 +36,40 @@ struct Event
 
 int main()
 {
-    Time start_time;
-    cin >> start_time.hour;
-    cin >> start_time.minute;
-    cin >> start_time.second;
-    Time end_time;
-    cin >> end_time.hour;
-    cin >> end_time.minute;
-    cin >> end_time.second;
-    Time durr= start_time.get_diff()
-}
+    int i = 0;
+    int n;
+    cin >> n;
+    int h = 0, m = 0, s = 0;
+    while (i < n)
+    {
+       // Event ev;
+        // cin >> ev.title >> ev.organiser;
+        Time start_time;
+        cin >> start_time.hour >> start_time.minute >> start_time.second;
+        Time end_time;
+        cin >> end_time.hour >> end_time.minute >> end_time.second;
+        Time durr = start_time.get_diff(end_time);
+        h += durr.hour;
+        m += durr.minute;
+        s += durr.second;
+        i++;
+    }
+    while (m >= 60 || s >= 60)
+    {
+        if (m >= 60)
+        {
+            h++;
+            m -= 60;
+        }
+        if (s >= 60)
+        {
+            m++;
+            s -= 60;
+        }
 
+    }
+    cout << h << ',' << m << ',' << s;
+
+    
+    
+}

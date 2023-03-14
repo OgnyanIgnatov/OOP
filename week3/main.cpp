cae4784d2fdfa5b8ce5c0a9d2588b4a11d2ac011
OgnyanIgnatov <125972711+OgnyanIgnatov@users.noreply.github.com>
2023-03-14 13:01:19 +0200

    Update main.cpp

--- week3/main.cpp
+++ week3/main.cpp
@@ -1,17 +1,55 @@
 #include <iostream>
 #include "set.h"
+int const INITIAL_CAPACITY = 10;
+
+void Set::copy(int const* arr, int numElements, int capacity)
+{
+	this->capacity = capacity;
+	this->numElements = numElements;
+
+	if (this->arr != nullptr)
+	{
+		delete[] this->arr;
+	}
+	this->arr = new int[this->capacity];
+	for (int i = 0; i < this->numElements; i++)
+	{
+		this->arr[i] = arr[i];
+	}
+}
 
 Set::Set()
-{	
-	int capacity;
-	int* arr = new int[capacity];
+{
+	this->numElements = 0;
+	this->capacity = INITIAL_CAPACITY;
+	this->arr = new int[this->capacity];
 };
+
+Set::Set(Set const& other)
+{
+	this->copy(other.arr, other.numElements, other.capacity);
+}
+
+Set Set::operator=(Set const& other)
+{
+	if (this != &other)
+	{
+		this->copy(other.arr, other.numElements, other.capacity);
+	}
+
+	return *this;
+}
+
+Set::~Set()
+{
+	delete[] this->arr;
+}
 
 void Set::resizeArr()
 {
 	this->capacity *= 2;
 	int* copyArr = new int[this->capacity];
-	for (int i = 0; i < this->capacity/2; i++)
+	for (int i = 0; i < this->capacity / 2; i++)
 	{
 		copyArr[i] = this->arr[i];
 	}
@@ -21,49 +59,73 @@ void Set::resizeArr()
 
 bool Set::addElement(const int element)
 {
-	bool a = false;
+	if (this->numElements == this->capacity)
+	{
+		this->resizeArr();
+	}
 
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < this->numElements; i++)
 	{
 		if (arr[i] == element)
 		{
-			return a;
+			return false;
 		}
 	}
-
-	if (!a)
-	{
-		a = true;
-		
-	}
+	this->arr[this->numElements] = element;
+	this->numElements--;
+	return true;
 }
 
 bool Set::deleteElement(const int element)
 {
-	bool a = false;
-
 	for (int i = 0; i < capacity; i++)
 	{
 		if (arr[i] == element)
 		{
-			delete arr[i];
-			return !a;
+			for (int j = i + 1; j < this->numElements; i++)
+			{
+				this->arr[j - 1] = this->arr[j];
+			}
+			this->numElements--;
 		}
 	}
 
-	return a;
+	return false;
 }
 
+void Set::setUnion(Set const& other)
+{
+	for (int i = 0; i < this->numElements; i++)
+	{
+		this->addElement(other.arr[i]);
+	}
+}
 
+void Set::setIntersection(Set const& other)
+{
+	for (int i = 0; i < this->numElements; i++)
+	{
+		int j = 0;
+		while (j < other.numElements && this->arr[i] != other.arr[j])
+		{
+			j++;
+		}
+		if (j == other.numElements)
+		{
+			this->deleteElement(this->arr[i]);
+			i--;
+		}
+	}
+}
 
-
-
-
-
-
-
-
-
+void Set::print()
+{
+	for (int i = 0; i < this->numElements; i++)
+	{
+		std::cout << this->arr[i] << " ";
+	}
+	std::cout << std::endl;
+}
 
 
 int main()

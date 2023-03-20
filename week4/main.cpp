58a9c3ebd7867a0a2bdb5fc85ee05296dbc932fe
OgnyanIgnatov <125972711+OgnyanIgnatov@users.noreply.github.com>
2023-03-20 16:28:29 +0200

    Update main.cpp
    Adding the rest of the functions

--- week4/main.cpp
+++ week4/main.cpp
@@ -40,10 +40,29 @@ Set Set::operator=(Set const& other)
 	return *this;
 }
 
+Set& Set::operator=(Set&& other)
+{
+	Set::Set(other);
+	return *this;
+}
+
 Set::~Set()
 {
 	delete[] this->arr;
 }
+
+
+Set::Set(Set&& other) :arr(nullptr), numElements(0), capacity(0)
+{
+	capacity = other.capacity;
+	numElements = other.numElements;
+	arr = other.arr;
+	
+	other.arr = nullptr;
+	other.numElements = 0;
+	other.capacity = 0;
+}
+
 
 void Set::resizeArr()
 {
@@ -152,16 +171,90 @@ void Set::multElemsByNums(const int num)
 
 Set Set::operator-(Set const& other)
 {
-	int* newArr = new int[this->numElements];
 	Set ob;
+	bool a = false;
 	for (int i = 0; i < this->numElements; i++)
 	{
-		for(i)
+		a = false;
+		for (int j = 0; j < other.numElements; j++)
+		{
+			if (this->arr[i] == other.arr[j])
+			{
+				a = true;
+			}
+		}
+		if (a == false)
+		{
+			ob.arr[i] = this->arr[i];
+		}
 	}
+	return ob;
 }
 
 
+Set Set::operator%(Set const& other)
+{
+	Set ob;
+	for (int i = 0; i < this->numElements; i++)
+	{
+
+		for (int j = 0; j < other.numElements; j++)
+		{
+
+			if (this->arr[i] == other.arr[j])
+			{
+				ob.arr[i] = this->arr[i];
+			}
+
+		}
+
+	}
+	return ob;
+}
+
+bool Contains(const int elem, int* arr, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		if (elem == arr[i])
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+bool Set::operator<=(Set const& other)
+{
+	int counter = 0;
+	for (int i = 0; i < this->numElements; i++)
+	{
+		if (Contains(arr[i], other.arr, other.numElements))
+		{
+			counter++;
+		}
+	}
+	if (counter == this->numElements)
+	{
+		return true;
+	}
+	return false;
+}
+
+
+bool Set::operator[](const int num)
+{
+	for (int i = 0; i < this->numElements; i++)
+	{
+		if (this->arr[i] == num)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
 int main()
 {
-
+	
 }

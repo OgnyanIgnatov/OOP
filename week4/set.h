523d6d2ef4f36630ce096ded54dd9141ee845fa1
OgnyanIgnatov <125972711+OgnyanIgnatov@users.noreply.github.com>
2023-03-20 16:29:18 +0200

    Update set.h
    Defining the rest of the fucntions

--- week4/set.h
+++ week4/set.h
@@ -19,7 +19,12 @@ public:
 	Set();
 	Set(Set const& other);
 	~Set();
+	Set(Set&& other);
 	Set operator=(Set const& other);
+	Set& operator=(Set&& other);
+	Set operator%(Set const& other);
+	bool operator<=(Set const& other);
+	bool operator[](const int num);
 	bool addElement(const int element);
 	bool deleteElement(const int element);
 	void print();

4aeec37041cd82adbc0a647a8c4607b6696fd14a
OgnyanIgnatov <125972711+OgnyanIgnatov@users.noreply.github.com>
2023-05-16 19:11:30 +0300

    Update ConsoleApplication1605.cpp

--- week12/ConsoleApplication1605.cpp
+++ week12/ConsoleApplication1605.cpp
@@ -5,11 +5,10 @@
 
 int main()
 {
-    char o[] = "1234";
-    User a("Pesho", o);
+    User a("Pesho", "1234");
     char* b = a.getName();
     char* c = a.getPassword();
-    for (int i = 0; i < strlen(a.getName()); i++)
+    for (int i = 0; i < 5; i++)
     {
         std::cout << b[i];
     }
@@ -18,6 +17,5 @@ int main()
     {
         std::cout << c[i];
     }
-    
-}
 
+}

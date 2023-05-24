3e1d018f4fc8f2683efd94614cfad64fdd13ebfc
OgnyanIgnatov <125972711+OgnyanIgnatov@users.noreply.github.com>
2023-05-24 13:49:26 +0300

    Update ConsoleApplication2305.cpp

--- week13/ConsoleApplication2305.cpp
+++ week13/ConsoleApplication2305.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
-
-
-
+#include "milkraisins.h"
+#include "raisins.h"
+#include "blackchoc.h"
+#include "chocolate.h"
+#include "milkchoc.h"
+#include "blackraisins.h"
 
 int main()
 {
+    MilkRaisins a(45, 17.5, 5);
+    std::cout << a.getID();
+    BlackRaisins b("NZ", 55, 9);
+    std::cout << b.getID();
+
     
 }
+
 
